1bc8cb10d7612b47361ae08c75199e5ae15bba8a
okihara <okihara@gmail.com>
2025-06-22 07:22:09 +0900

    remove redundant calendar read-only scope from SCOPES

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ CLIENT_CONFIG = {
 }
 
 # スコープの設定
-SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/tasks']
+SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/tasks']
 
 # PostgreSQLへの接続
 def get_db_connection():
